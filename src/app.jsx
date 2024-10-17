fb513e68f626e84233238807f244460481a8a611
andreavillartr <andreavillartr@gmail.com>
2024-10-17 19:20:32 +0000

    Base frontend app

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>ConstruHack 2024</h1>
+      {/* Aquí irá el contenido de tu aplicación */}
+    </div>
+  );
+}
+
+export default App;
