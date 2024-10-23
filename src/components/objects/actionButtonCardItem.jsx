e089c178d26e209c2b200a62ba65143cb1caa1cc
Andrea <144063021+AndreaKinder@users.noreply.github.com>
2024-10-23 14:51:48 +0200

    update action botton caard item

--- src/components/objects/actionButtonCardItem.jsx
+++ src/components/objects/actionButtonCardItem.jsx
@@ -4,8 +4,8 @@ import styles from "../styles";
 
 export default function ActionButtonCardItem() {
     return (
-        <View style={styles.actions}>
-          <Button title="Reservar | Solicitar" onPress={() => console.log('Reservar | Solicitar')} />
-        </View>
+    <View style={{ marginBottom: 10 }}>
+      <Button styles={styles.button} title="Reservar | Solicitar" onPress={() => console.log('Reservar | Solicitar')} />
+    </View>
     )
 }
